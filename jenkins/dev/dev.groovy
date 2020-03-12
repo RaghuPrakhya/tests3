be806c0a5efbcba980454ef951cd7b18d7a442d9
EC2 Default User <ec2-user@ip-172-31-20-197.ec2.internal>
2020-03-12 23:42:18 +0000

    Testting Groovy

--- jenkins/dev/dev.groovy
+++ jenkins/dev/dev.groovy
@@ -2,16 +2,20 @@ pipeline{
 
     agent { label "master" }
 
+    parameters {       
+        string(name: 'ACCOUNTS', defaultValue: 'dev-raghu,dev-raghu2', description: 'Comma separated list of accounts to deploy to')
+    }
+
     environment {
         // Parent folder and deploy env will often be the same
         PARENT_FOLDER           = 'dev'
-        DEPLOY_ENVIRONMENT      = 'dev'        
+        DEPLOY_ENVIRONMENT      = 'dev'
         IS_JENKINS_MODE         = "true"
-        CI                      = "true"
         GIT_REPO                = "https://github.com/RaghuPrakhya/tests3.git"
         GIT_CREDENTIALS         = "RaghuPrakhya@gmail.com"
         // Recipient of the notification emails
         EMAIL_RECIPIENT = 'RaghuPrakhya@gmail.com' // Multiple emails can be separated by a semi-colon
+
     }
 
     stages {
@@ -37,99 +41,80 @@ pipeline{
 
 
 
-        /*
-            Because the Jenkins server exists in prod we must assume a role into the environment that we want to interact with.
-            This will cause all commands we run after this to be executed against that environment instead of prod.
-            After assuming a role if you need to execute commands against prod again you must assume your role back into production.
-            If after assumeing a role you need to assume a role into another account/environment you must assume the role back into production first since production is the only one allowed to assume roles into other accounts.
-            If you needed to assume a role in dev and then test it would look something like this:
-            - Assume role in dev
-            - Execute dev commands
-            - Assume role in prod
-            - Assume role in test
-            - Execute test commands
-        */
-//        stage('Assume Role') {
-//            steps {
-//                // Set AWS Credentials
-//                script {
-//                    env.STSRESPONSE=sh(returnStdout: true, script: "aws sts assume-role --role-arn arn:aws:iam::178375044839:role/baxaws-sandbox-operator-admin --role-session-name jenkins")
-//                    env.AWS_ACCESS_KEY_ID = sh(returnStdout: true, script: "echo \$STSRESPONSE | jq -r .Credentials.AccessKeyId").trim()
-//                    env.AWS_SECRET_ACCESS_KEY = sh (returnStdout: true, script: "echo \$STSRESPONSE | jq -r .Credentials.SecretAccessKey").trim()
-//                    env.AWS_SESSION_TOKEN = sh (returnStdout: true, script: "echo \$STSRESPONSE | jq -r .Credentials.SessionToken").trim()
-//                    echo "Assume stage"
-//                }
-//            }
-//            // post section to trigger email on failure
-//            post {
-//                failure {
-//                    script {
-//                        currentBuild.result = 'FAILURE'
-//                        notifyBuild(currentBuild.result)
-//                    }
-//                }
-//            }
-//        }
+          stage("Execute on all accounts") {
+              steps {
+                  sh '''
+                      echo "Path is $PATH"
+                      OIFS=$IFS;
+                      IFS=",";
+                      
+                      ACCOUNTS_ARRAY=($ACCOUNTS);
+                      WORKSPACE=$(pwd);
+  
+                      ## Loop through all the accounts provided
+                      for ((i=0; i<${#ACCOUNTS_ARRAY[@]}; ++i)); do     
+                      
+                          ## For each account determine the account number
+                          if [ ${ACCOUNTS_ARRAY[$i]} == 'dev-raghu' ]; then
+                              ACCOUNT_NUM='770765425423'
+                          elif [ ${ACCOUNTS_ARRAY[$i]} == 'dev-raghu2' ]; then
+                              ACCOUNT_NUM='770765425423'
+                          fi
+  
+                          echo "${ACCOUNTS_ARRAY[$i]} : $ACCOUNT_NUM"
+  
+                          ## Assume a role into the dev account
+                          STSRESPONSE=$(aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT_NUM}:role/cloudformation-role --role-session-name jenkins)
+                          AWS_ACCESS_KEY_ID=$(echo "${STSRESPONSE}" | jq '.Credentials.AccessKeyId' | cut -d'"' -f2)
+                          AWS_SECRET_ACCESS_KEY=$(echo "${STSRESPONSE}" | jq '.Credentials.SecretAccessKey' | cut -d'"' -f2)
+                          AWS_SESSION_TOKEN=$(echo "${STSRESPONSE}" | jq '.Credentials.SessionToken' | cut -d'"' -f2)
+                          export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+                          export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+                          export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
 
-        /*
-            https://github.com/onicagroup/runway/
-            Runway is a wrapper for cloudformation.
-            This wrapper allows us to use this standard directory structure to deploy infrastructure.
-            It also allows use to use environment files to extract away the differences between environments and have a standard deployment to every environment
-            Runway has a few commands:
-            - runway test (aka runway preflight) - execute this in your environment to catch errors; if it exits 0, you're ready for...
-            - runway plan (aka runway taxi) - this optional step will show the diff/plan of what will be changed. With a satisfactory plan you can...
-            - runway deploy (aka runway takeoff) - if running interactively, you can choose which deployment to run; otherwise (i.e. on your CI system) each deployment will be run in sequence.
-        */
-        stage ('Preflight Check') {
-            steps {
-               
-                sh 'cd deployments/${PARENT_FOLDER} && runway plan 2>&1 | tee plan.txt; chngCnt=`grep \'full changeset\' plan.txt | wc -l `; echo "Add count is $chngCnt"; if [[ $chngCnt -gt 0 ]]; then exit 0; else echo exit 1; fi'
-            }
-            // post section to trigger email on failure
-            post {
-                failure {
-                    script {
-                        currentBuild.result = 'FAILURE'
-                        notifyBuild(currentBuild.result)
-                    }
-                }
-            }
-        }
+                          echo ${AWS_ACCESS_KEY_ID}
+                          echo ${AWS_SECRET_ACCESS_KEY}
+                          echo ${AWS_SESSION_TOKEN} 
 
-        /*
-            https://github.com/onicagroup/runway/
-            Runway is a wrapper for cloudformation.
-            This wrapper allows us to use this standard directory structure to deploy infrastructure.
-            It also allows use to use environment files to extract away the differences between environments and have a standard deployment to every environment
-            Runway has a few commands:
-            - runway test (aka runway preflight) - execute this in your environment to catch errors; if it exits 0, you're ready for...
-            - runway plan (aka runway taxi) - this optional step will show the diff/plan of what will be changed. With a satisfactory plan you can...
-            - runway deploy (aka runway takeoff) - if running interactively, you can choose which deployment to run; otherwise (i.e. on your CI system) each deployment will be run in sequence.
-        */
-        stage ('Deploy') {
-            steps {
-                //sh "cd deployments/${PARENT_FOLDER} && runway deploy"
-                sh "cd deployments/${PARENT_FOLDER}"
-		echo "cd deployments/${PARENT_FOLDER}"
-            }
-            // post section to trigger email on failure
-            post {
-                failure {
-                    script {
-                        currentBuild.result = 'FAILURE'
-                        notifyBuild(currentBuild.result)
-                    }
-                }
-            }
-        }
+                          ######################################
+                          #### Take deployment actions here ####
+                          ######################################
+                          
+                          #echo $WORKSPACE
+                          #cd $WORKSPACE/runway/dev
+                          #cd /var/lib/jenkins/workspace/Security/dev/deploy-config-rules/runway/dev
+                          export DEPLOY_ENVIRONMENT=${ACCOUNTS_ARRAY[$i]}
+                          #runway taxi
+                          #runway takeoff
+                          cd /var/lib/jenkins/workspace/tests3
+                          aws s3 ls
+  
+                          ################################
+                          #### End deployment actions ####
+                          #################################
+  
+                          ## Assume role back to prod. Prod is the only account that can assume roles
+                          STSRESPONSE=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/JenkinsRole)
+                          AWS_ACCESS_KEY_ID=$(echo "${STSRESPONSE}" | jq '.AccessKeyId' | cut -d'"' -f2 )
+                          AWS_SECRET_ACCESS_KEY=$(echo "${STSRESPONSE}" | jq '.SecretAccessKey' | cut -d'"' -f2)
+                          AWS_SESSION_TOKEN=$(echo "${STSRESPONSE}" | jq '.Token' | cut -d'"' -f2)
+                          export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+                          export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+                          export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
+                      done
+  
+                      ## Reset IFS variable at the end
+                      IFS=$OIFS;
+                  '''
+              }
+          }
 
-        stage('Send email') {
+          stage('Send email') {
             steps {
                 script {
                     // Set build result and trigger email for successful build
                     currentBuild.result = 'SUCCESS'
-                    notifyBuild(currentBuild.result)
+                    // notifyBuild(currentBuild.result)
                 }
             }
         }         
@@ -149,3 +134,4 @@ def notifyBuild(String buildStatus = 'STARTED') {
         """
     )
 }
+
